6ecc11700a3ae7c985faa761cbd7260e8f4c278d
hslee <vawav11@gmail.com>
2024-04-28 14:33:18 +0900

    이미지 로드

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+          body: GradientContainer(
+        Color.fromARGB(255, 26, 2, 80),
+        Color.fromARGB(255, 45, 7, 98),
+      )),
     ),
   );
 }
