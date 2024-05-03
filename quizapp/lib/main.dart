f4302d1f287f329124977f30bd0596038bccfabb
hslee <vawav11@gmail.com>
2024-05-03 20:41:23 +0900

    quiz, question_screen 생성

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/start_screen.dart';
+import 'package:quizapp/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 46, 166, 142),
-                Color.fromARGB(255, 101, 209, 187),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ quizapp/lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 46, 166, 142),
+                Color.fromARGB(255, 101, 209, 187),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
